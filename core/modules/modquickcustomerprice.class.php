a144e31db7b7a51c7509c1313547b3155e24ec9c
Adrien Raze <adrien.raze@atm-consulting.fr>
2024-12-05 10:23:10 +0100

    FIX : retrocompatibility

--- core/modules/modquickcustomerprice.class.php
+++ core/modules/modquickcustomerprice.class.php
@@ -112,7 +112,7 @@ class modquickcustomerprice extends DolibarrModules
 		$this->requiredby = array();	// List of modules id to disable if this one is disabled
 		$this->conflictwith = array();	// List of modules id this module is in conflict with
 		$this->phpmin = array(7,1);					// Minimum version of PHP required by module
-		$this->need_dolibarr_version = array(21,0);	// Minimum version of Dolibarr required by module
+		$this->need_dolibarr_version = array(16,0);	// Minimum version of Dolibarr required by module
 		$this->langfiles = array("quickcustomerprice@quickcustomerprice");
 
 		// Constants

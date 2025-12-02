59462fc7cb20612c069776dc6dae0132b6ac5ed1
ATM-Lucas <lucas.mantegari@atm-consulting.fr>
2025-12-02 12:26:33 +0100

    changlog + version

--- core/modules/modquickcustomerprice.class.php
+++ core/modules/modquickcustomerprice.class.php
@@ -60,7 +60,7 @@ class modquickcustomerprice extends DolibarrModules
 		$this->description = "Modify a value of your product or service with one click";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 
-		$this->version = '3.8.10';
+		$this->version = '3.8.11';
 
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
